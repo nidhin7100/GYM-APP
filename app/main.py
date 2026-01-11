5617093fa81334a1dae1f8b5df4a771c2b73c2e5
Nidhin <nidhinbabumadathil@gmail.com>
2026-01-11 22:53:55 +0530

    Enable CORS for web

--- app/main.py
+++ app/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .database import Base, engine
 from .routers import workouts, sessions, sets
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "*",  # Allow all origins (for testing). Later you can restrict to your domain.
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(workouts.router, prefix="/workouts")
 app.include_router(sessions.router, prefix="/sessions")
